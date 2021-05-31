eeb222ed0e1f1ae5c4bdb5a7fe305046c3572ce6
Abigail M-A <a.mackenzie.armes@gmail.com>
2021-05-31 10:07:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import React, { Component } from 'react'
 import {Route, Link, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
-import NotefulContext from './NotefulContext'
+import FurFriendlyContext from './FurFriendlyContext'
 
-import NavNewNoteFolder from './NavNewNoteFolder'
-import NavList from './NavList'
-import NavNoteView from './NavNoteView'
+import Space from './Space';
+import SpaceContainer from './SpaceContainer'
+import AddSpace from './AddSpace'
 
-import NoteListAll from './NoteListAll'
-import NoteListFolder from './NoteListFolder'
-import AddFolder from './AddFolder'
-import AddNote from './AddNote'
-import Note from './Note';
-import ErrorBoundary from './ErrorBoundary'
-
-//import STORE from './dummy-store';
+import STORE from './dummy-store';
 
 export default class App extends Component {
-    static contextType = NotefulContext;
+    static contextType = FurFriendlyContext;
     state = {
-        notes: [],
-        folders: [],
+        spaces: []
     }
-
+    /*
     componentDidMount(){
         fetch('https://obscure-hollows-57839.herokuapp.com/notes')
         .then(notesResult => {
@@ -56,63 +48,6 @@ export default class App extends Component {
         .catch(error =>
             console.log(error.message)
         )
-    }
-
-    deleteNote = noteId => {
-        console.log('Deleting Note: '+noteId)
-        const newNotes = this.state.notes.filter(note => note.id !== noteId)
-        this.setState({
-            notes: newNotes
-        });
-
-        fetch(`https://obscure-hollows-57839.herokuapp.com/notes/${noteId}`, {
-            method: 'DELETE',
-            headers: {
-                'content-type': 'application/json'
-            },
-        })
-        .then(result => {
-            if(!result.ok){
-                throw new Error('Something went wrong.')
-            }
-            return result.json()
-        })
-        .catch(error => {
-            console.log(error.message)
-        })
-    };
-
-    addFolder = folderName => {
-        console.log('Adding Folder: ' + folderName);
-        const oldFolders = this.state.folders
-        oldFolders.push({
-            "id":`${this.generateID()}-ffaf-11e8-8eb2-f2801f1b9fd1`,
-            'name': folderName
-        });
-        this.setState({
-            folder: oldFolders
-        })
-
-        fetch(`https://obscure-hollows-57839.herokuapp.com/folders/`, {
-            method: 'POST',
-            headers: {
-              'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                "folderid":`${this.generateID()}-ffaf-11e8-8eb2-f2801f1b9fd1`,
-                'name': folderName
-            })
-          }
-        )
-        .then(result => {
-            if(!result.ok){
-                throw new Error('Something went wrong.')
-            }
-            return result.json()
-        })
-        .catch(error => {
-            console.log(error.message)
-        })
     }
 
     addNote = (noteName,noteText,noteFolder) => {
@@ -168,57 +103,26 @@ export default class App extends Component {
         }
         return id;
     }
-    
-    mainRoutes = () => {
-        return(
-            <>
-                <Route exact path='/' component={NoteListAll}/>
-                <Route exact path='/folders' component={NoteListAll}/>
-                <Route exact path='/folders/:folderId' component={NoteListFolder}/>
-                <Route path='/folders/:folderId/notes/:noteId' component={Note}/>
-                <Route path='/add-folder' component={AddFolder}/>
-                <Route path='/add-note' component={AddNote}/>
-            </>
-        )
-    }
-
-    navRoutes = () => {
-        return(
-            <>
-                <Route exact path='/' component={NavList}/>
-                <Route exact path='/folders' component={NavList}/>
-                <Route exact path='/folders/:folderId' component={NavList}/>
-                <Route path='/folders/:folderId/notes/:noteId' component={NavNoteView}/>
-                <Route path='/add' component={NavNewNoteFolder}/>
-            </>
-        )
-    }
+    */
 
     render(){
         const contextValue = {
-            notes: this.state.notes,
-            folders: this.state.folders,
-            deleteNote: this.deleteNote,
-            addFolder: this.addFolder,
-            addNote: this.addNote
+            spaces: STORE.spaces,
+            types: STORE.types
         }
         return(
             <BrowserRouter>
-                <NotefulContext.Provider value={contextValue}>
+                <FurFriendlyContext.Provider value={contextValue}>
                     <header>
-                        <Link to='/'><h1>Noteful</h1></Link>
+                        <Link to='/'><h1>Fur Friendly Spaces</h1></Link>
                     </header>
                     <div className='app'>
-                            <nav>
-                                <ErrorBoundary>
-                                    {this.navRoutes()}
-                                </ErrorBoundary>
-                            </nav>
                             <main>
-                                {this.mainRoutes()}
+                                <AddSpace />
+                                <SpaceContainer />
                             </main>
                     </div>
-                </NotefulContext.Provider>
+                </FurFriendlyContext.Provider>
             </BrowserRouter>
             
         )
